c79c9bfa355ba95e384780ea61e23f9e92fbf2a7
paulara <paulara99@gmail.com>
2017-01-27 12:37:35 +0100

    ejercicios no hechos, solo para guardar anteriores y poder modificarlos

--- cu/ejer1.py
+++ cu/ejer1.py
@@ -1,22 +1,51 @@
+"Crear clase cuenta"
 class Cuenta(object):
-    saldo= 0.0
-    def __init__(self, saldo):
-        self.saldo= saldo
+    def __init__(self,__saldo,contrasena):
+        self.__saldo=0.0
+        self.contrasena=contrasena
     def ingresar (self,cantidades):
-        self.saldo=self.saldo+cantidades
-        print(self.saldo)
+        self.__saldo=self.__saldo+cantidades
+        print("saldo actual:",self.__saldo)
     def retirar (self,cantidades2):
-        if cantidades2<self.saldo:
-            self.saldo-=cantidades2
+            self.__saldo-=cantidades2
+            print("saldo actual:",self.__saldo) 
+    def leer_saldo(self,contrasena):  
+        if self.contrasena==contrasena:
+            print("saldo actual",self.__saldo)
         else:
-            print("Su saldo no es suficiente")
-        print(self.saldo)
-cuenta1=Cuenta(0.0)
-cantidades=[125.23,503.4,50.0]
-for elemento in range(3):
-    cuenta1.ingresar(cantidades[elemento])
-print("cantidad ingresada",cantidades)
-cantidades2=[333.34]
-for elemento in range(1):
-    cuenta1.retirar(cantidades2[elemento])
-print("cantidad retirada",cantidades2)
+            print("Contrasena incorrecta")
+    def saldonooculto(self):
+        return self.__saldo
+"Crear clase cuenta de ahorro"            
+class Cuenta_ahorro(Cuenta):
+    def __avisar(self,cantidades2): 
+        if  cantidades2>super(Cuenta_ahorro,self).saldonooculto():
+            print("Numeros rojos")
+        else:
+            super(Cuenta_ahorro,self).retirar(cantidades2)
+    def llamaravisar(self,cantidades2):
+        self.__avisar(cantidades2)
+"Programa principal"    
+if __name__ == '__main__':
+    "Todo con objetocuenta1"
+    cuenta1=Cuenta(0.0,"3210")
+    cantidades=[300]
+    for elemento in range(1):
+        print("cantidad ingresada",cantidades)
+        cuenta1.ingresar(cantidades[elemento])
+    cantidades2=[290,20,30]
+    for elemento in range(3):
+        print("cantidad retirada",cantidades2[elemento])
+        cuenta1.retirar(cantidades2[elemento])
+    contrasena=input("Introduzca contrasena")
+    cuenta1.leer_saldo(contrasena)
+    "Cuenta2"
+    cuenta2=Cuenta_ahorro(0.0,"3210")
+    cantidades=[300]
+    for elemento in range(1):
+        print("cantidad ingresada",cantidades)
+        cuenta2.ingresar(cantidades[elemento])
+    cantidades2=[290,20,30]
+    for elemento in range(3):
+        print("cantidad retirada",cantidades2[elemento])
+        cuenta2.llamaravisar(cantidades2[elemento])
--- cu/ejer2.py
+++ cu/ejer2.py
@@ -1,9 +1,5 @@
+from ejer1 import Cuenta_ahorro
 class Persona(object):
-    nombre=''
-    dni=''
-    direccion=''
-    telefono=''
-    email=''
     def __init__(self,nombre,dni,direccion,telefono,email):
         self.nombre=nombre
         self.dni=dni
@@ -14,18 +10,27 @@ class Persona(object):
         print("nombre",self.nombre,"dni",self.dni,"direccion",self.direccion,"telefono",self.telefono)
         if self.email!='':
             print("email",self.email)
-    def nombrar(self,):
-        print(self.nombre)
-persona1=Persona('John',77139012,'granada',987654321,'nono')
-persona2=Persona('Lara',603458060,'finlandia',958439773,'lala')
-persona3=Persona('Lolo',77136980,'Granada',987654321,'lolailo@gmail.com')
-persona4=Persona('Paula',77139013,'Atarfe',958172252,'paulara99@gmail.com')
-persona1.mostrar()
-persona2.mostrar()
-persona3.mostrar()
-persona4.mostrar()
-print('Nombre alumnos')
-persona1.nombrar()
-persona2.nombrar()
-persona3.nombrar()
-persona4.nombrar()
+        else:
+            print("no hay email")
+class Cliente(Persona,Cuenta_ahorro):
+    def __init__(self,saldo,contrasena,nombre,dni,direccion,telefono,email,credit):
+        Cuenta_ahorro.__init__(self,saldo,contrasena)
+        Persona.__init__(self, nombre, dni, direccion, telefono, email)
+        self.credit=1000
+    def mostrar_datos(self):
+        Persona.mostrar(self)
+if __name__ == '__main__':
+    "ejercicio anterior"
+    clase_tic=[]
+    for n in range (10):
+        nombre=input("Introduzca el nombre")
+        dni=input("Introduzca el dni")
+        direccion=input("Introduzca la direccion")
+        telefono=input("introduzca el telefono")
+        email=input("Introduzca el correo")
+        clase_tic.append(Persona(nombre,dni,direccion,telefono,email))
+    for i in range(10):
+        clase_tic[i].mostrar()
+    "Para ejercicio 3"
+    cliente=Cliente(2500,"3210","Mariano Fuentes","45365968L","C/ de las Alpujarras 2, bajo",958776566, "mfuentes@iaa.es",1000)
+    cliente.mostrar_datos(saldo,nombre,dni,direccion,telefono,email,credit)
